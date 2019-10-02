158d45e479e8cba398412c15c227f8a16e9ca91c
Greg Wiley <gwiley@cj.com>
2019-10-02 13:43:20 -0700

    a bukkit

--- lib/cj-static-stack.ts
+++ lib/cj-static-stack.ts
@@ -1,9 +1,13 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
 
 export class CjStaticStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'Content');
+
   }
 }
+
+
