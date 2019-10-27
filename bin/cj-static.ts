16da45783fa498c3fdeed1163757b4f424dc01ae
Greg Wiley <gwiley@cj.com>
2019-10-27 14:31:18 -0700

    one dist for now

--- bin/cj-static.ts
+++ bin/cj-static.ts
@@ -8,4 +8,4 @@ import cdk = require('@aws-cdk/core');
 const app = new cdk.App();
 const sites = new StaticSites(app, "StaticSites");
 sites.addDistribution("files.cj.dev");
-sites.addDistribution("commitments.cj.dev");
+// sites.addDistribution("commitments.cj.dev");
